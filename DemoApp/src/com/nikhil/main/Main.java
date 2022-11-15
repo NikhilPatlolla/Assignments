3990f26e9ab12812eb9be1e9cf8639d6c8afb868
patloln <patloln@MININT-T8KOICE.ant.amazon.com>
2022-11-16 01:47:21 +0530

    First Commit

---
+++ DemoApp/src/com/nikhil/main/Main.java
@@ -0,0 +1,15 @@
+package com.nikhil.main;
+
+public class Main {
+	public static void main(String[] args) {
+		int[] a = {10, 20, 34, 45, 67};
+		for (int i : a) {
+			System.out.println(i);
+		}
+		
+		
+		String name = "sachin";
+		int count = name.length();
+		System.out.println("The length of the String is:: " + count);
+	}
+}
